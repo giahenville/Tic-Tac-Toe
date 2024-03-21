73249757e18d6ad7ddb13e7cc5b47452c13241f9
Gia Henville <140769584+giabui@users.noreply.github.com>
2024-03-20 21:39:51 -0400

    feat: Add basic game layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const gameBoardArr = [];
+const playersObj = {};
+
 
 function App() {
   return (
- <h1 className="underline"> 
-  Hello Gia!
- </h1>
+  <>
+    <div className='header'>
+      <p className='difficulty-level underline'>Mode</p>
+      <span>
+        <button className='X border-solid border-2 border-black'> X </button>
+        <button className='O border-solid border-2 border-black'> O </button>
+      </span>
+    </div>
+
+    <div className='grid bg-green-100'>
+      X X O 
+      O O X 
+      O O X
+    </div>
+    <footer>Start / Restart</footer>
+    
+
+    
+  </>
+ 
   );
 }
 
