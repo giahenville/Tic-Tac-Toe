30ba39bf7c58d930af8c530057983c3cdf783f10
Gia Henville <140769584+giabui@users.noreply.github.com>
2024-03-20 22:11:39 -0400

    feat: allow for clicking 'x' or 'o' to update grid

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+// import './Grid.css';
+
 
 const gameBoardArr = [];
 const playersObj = {};
 
 
+
 function App() {
+
+  const [currentValue, setCurrentValue] = useState(null);
+  const handleXClick = () => {
+    setCurrentValue("X");
+  }
+  const handleOClick = () => {
+    setCurrentValue("O")
+  }
+
   return (
   <>
     <div className='header'>
       <p className='difficulty-level underline'>Mode</p>
       <span>
-        <button className='X border-solid border-2 border-black'> X </button>
-        <button className='O border-solid border-2 border-black'> O </button>
+        <button className='X border-solid border-2 border-black' onClick={handleXClick}> X </button>
+        <button className='O border-solid border-2 border-black'  onClick={handleOClick}> O </button>
       </span>
     </div>
 
-    <div className='grid bg-green-100'>
-      X X O 
-      O O X 
-      O O X
+    <div className='main-body grid grid-rows-3 grid-flow-col bg-green-100 w-5rem'>
+      <div className='sq1 border-solid border-2 border-black'>{currentValue}</div>
+      <div className='sq2 border-solid border-2 border-black'>{currentValue}</div>
+      <div className='sq3 border-solid border-2 border-black'>{currentValue}</div>
+      <div className='sq4 border-solid border-2 border-black'>{currentValue}</div>
+      <div className='sq5 border-solid border-2 border-black'>{currentValue}</div>
+      <div className='sq6 border-solid border-2 border-black'>{currentValue}</div>
+      <div className='sq7 border-solid border-2 border-black'>{currentValue}</div>
+      <div className='sq8 border-solid border-2 border-black'>{currentValue}</div>
+      <div className='sq9 border-solid border-2 border-black'>{currentValue}</div>
+
     </div>
-    <footer>Start / Restart</footer>
+
+    <footer>
+      <button>Start / Restart</button>
+    </footer>
     
 
     
